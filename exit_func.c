38fae7033e049165b390511bcf7e3c48d37130a4
wishon1 <wisdomhonest1@gmail.com>
2023-09-09 11:16:46 -0400

    update to the project

---
+++ exit_func.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _exit - exit with a value of 1;
+ * @value: the integer to exit 1 on failure and 
+ * 0 on success
+ *
+ * @pointer - pointer to the array containing the command.
+ */
+void exit_1(int value, char *pointer)
+{
+	free (pointer);
+	exit(value);
+}
